1d75818358105530ed0adfb45983c658f98521b9
BobokBobok <125795711+BobokBobok@users.noreply.github.com>
2024-08-26 11:15:35 +1000

    Update test.py
    fixed normalisation bug by defining the spacings array and multiplying by it.

--- test.py
+++ test.py
@@ -7,32 +7,30 @@ from scipy import signal
 t = np.linspace(0, 20, 1000, endpoint=True)
 sawtooth = signal.sawtooth(0.5 * np.pi * t)
 
-#Function for Cauchy distributed RV
+'''Function for Cauchy distributed RV'''
 def CauchyDist(x,Gamma):
     b = Gamma/2
     m = np.max(x)/2
-    dx1 = np.array(abs(x[1]-x[0]))
-    dxs = np.diff(x)
-    dx = np.concatenate((np.array([dx1]),dxs))
-    return 1/np.pi * (b/((x-m)**2+b**2)) * dx
+    return 1/np.pi * (b/((x-m)**2+b**2))
 
 def imfpConv(mu,a):
     IMFP = np.zeros((len(mu),len(mu))) #Allocate memory for square IMFP matrix
-    #Generate matrix to convolve
+    '''Generate matrix to convolve'''
     for i in range(0,len(mu)):
         IMFP[i,0:len(mu)] += mu #Rows are the gamma hole convolved data
             
     CONV = np.zeros((len(a),len(mu)))#Allocate memory for convolution matrix
-        #Fill in convolution matrix
+    '''Fill in convolution matrix'''
     for i in range(0,len(a)):
         y = CauchyDist(t,a[i]) #Each row of the convolution matrix is Cauchy distributed with width a(E)
-            #print(a[i])
-            #plt.plot(E,y)
         CONV[i,0:len(mu)] += y
-            
-            #Convolve the data row wise, this produces a matrix in which each row is a convoolution of the data due to a different width a(E). Return the diagonal of this matrix.
+        
+    '''For normalisation of the convolution, create a spacing array'''
+    spacing = np.diff(Energy,prepend=0.)
+    spacing[0]=Energy[1]-Energy[0]
+    '''Convolve the data row wise, this produces a matrix in which each row is a convoolution of the data due to a different width a(E). Return the diagonal of this matrix weighted by the spacings.'''
     CONVOLVED = signal.fftconvolve(IMFP,CONV,mode = 'same', axes = 1)
-    return np.diagonal(CONVOLVED)
+    return np.diagonal(CONVOLVED)*spacing
      
 ##Convolve sawtooth function with a Lorenzian
 #Cauchy = CauchyDist(t,0.1)
